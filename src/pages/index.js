8d40c369f80ee9548e79a48b83ee0c7179e4330d
t-zouchi <mojamoja.000@gmail.com>
2021-01-05 17:57:59 +0900

    update top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
 const IndexPage = () => (
   <Layout>
-    <p></p>
+    <p>物取りの背景に使ってね.ページ開いてF11で最大画面にして写真を取ろう!</p>
+    <Link to="/gray" className="grayLink myLink">
+      グレー
+    </Link>
+    <br />
+    <Link to="/graygradient" className="grayGradientLink myLink">
+      グレーのグラデーション{" "}
+    </Link>
+    <br />
+    <Link to="/rainbow" className="rainbowLink myLink">
+      レインボー
+    </Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
